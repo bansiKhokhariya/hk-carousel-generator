3a6e2e500c76a7859a8e02d8b7e28f8183b856f0
bansi-hkapps <bansi.hkapps@gmail.com>
2024-06-07 11:37:07 +0530

    change image path

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 const Hero = () => {
   return (
     <>
-      <div style={{ backgroundImage: "url('/images-banner/banner.jpg')", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+      <div style={{ backgroundImage: "url('/images-banner/Banner.jpg')", backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
         <section className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center justify-between lg:gap-20  pt-6 lg:pt-10">
           <div>
             <h1 className="text-[20px] font-bold !leading-[1.2] lg:text-4xl xl:text-[40px]">
@@ -11,7 +11,7 @@ const Hero = () => {
             {/* <TryFreeButton openModal={openModal} /> */}
           </div>
           <div className="w-1/2">
-            <img src={`${process.env.NEXT_PUBLIC_APP_URL}/images-banner/layer.png`} className="w-full h-auto object-contain" alt="LinkedIn Carousel Example" />
+            <img src={`${process.env.NEXT_PUBLIC_APP_URL}/images-banner/Layer.png`} className="w-full h-auto object-contain" alt="LinkedIn Carousel Example" />
           </div>
         </section>
       </div>
